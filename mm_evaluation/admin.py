4cf8971c21533a6f986bc90bb4bd7cd2969b23c0
Vincent Alejandro Arcila Larrea <jandro.vins@gmail.com>
2020-03-03 14:01:34 -0500

    Registered database objects

--- mm_evaluation/admin.py
+++ mm_evaluation/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sector, PYME, Autoevaluation, Macroprocess, Process, Answer, SpecificPractice, GeneralPractice, Archive, FinancesInformation
+
+admin.site.register(Sector)
+admin.site.register(PYME)
+admin.site.register(Autoevaluation)
+admin.site.register(Macroprocess)
+admin.site.register(Process)
+admin.site.register(Answer)
+admin.site.register(SpecificPractice)
+admin.site.register(GeneralPractice)
+admin.site.register(Archive)
+admin.site.register(FinancesInformation)
