88e85f32e87dae945be2d15b14a18b4c5c42841e
clopezr9 <clopezr9@eafit.edu.co>
2020-03-08 17:53:16 -0500

    models.py and admin.py corrected

--- mm_evaluation/admin.py
+++ mm_evaluation/admin.py
@@ -2,13 +2,8 @@ from django.contrib import admin
 
 from .models import Sector, PYME, Autoevaluation, Macroprocess, Process, Answer, SpecificPractice, GeneralPractice, Archive, FinancesInformation
 
-admin.site.register(Sector)
-admin.site.register(PYME)
-admin.site.register(Autoevaluation)
 admin.site.register(Macroprocess)
 admin.site.register(Process)
-admin.site.register(Answer)
 admin.site.register(SpecificPractice)
 admin.site.register(GeneralPractice)
-admin.site.register(Archive)
-admin.site.register(FinancesInformation)
+
