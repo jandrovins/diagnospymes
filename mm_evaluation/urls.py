9b850cecd419902e37e0addb624dbd52d12ef2ca
Vincent Alejandro Arcila Larrea <jandro.vins@gmail.com>
2020-03-08 21:31:19 -0500

    WIP

--- mm_evaluation/urls.py
+++ mm_evaluation/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'mm_evaluation'
 urlpatterns = [
-        path('', views.Autoevaluation.as_view()),
+        path('', views.Autoevaluation.as_view(), name='autoevaluation'),
+        path('save_process_score/', views.save_process_score(), name='save_process_score'),
+
 ]
--- mm_evaluation/views.py
+++ mm_evaluation/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView
-from .models import Process, Macroprocess
+from django.shortcuts import get_object_or_404
+from .models import Process, Macroprocess, Autoevaluation
 
 class Autoevaluation(ListView):
     model = Macroprocess
     template_name = 'mm_evaluation/autoevaluation.html'
     context_object_name = 'macroprocesses_list'
 
-    #def get_context_data(self, **kwargs):
-    #    # Call the base implementation first to get a context
-    #    context = super().get_context_data(**kwargs)
-    #    # Add in a QuerySet of all the books
-    #    context['process_list'] = Process.objects.all()
-    #    return context
+def view_results(request, autoevaluation_id):
+    autoevaluation = get_object_or_404(Autoevaluation, pk=autoevaluation_id)
+:   try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
