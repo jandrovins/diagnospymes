87bbf1d7c7337dadfbc28f69e3fee8294bdb2b9c
Vincent Alejandro Arcila Larrea <jandro.vins@gmail.com>
2020-03-10 13:22:22 -0500

    Filling first created (but not fully answered) autoevaluation by default

--- mm_evaluation/urls.py
+++ mm_evaluation/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'mm_evaluation'
 urlpatterns = [
         path('autoevaluation/', views.AutoevaluationView.as_view(), name='autoevaluation'),
-        path('<int:pk>/save_answer/', views.save_answer, name='save_answer'),
+        path('<int:pk>/save_answer/', views.AutoevaluationView.as_view(), name='save_answer'),
         path('process_already_answer/', views.ProcessAlreadyAnswerView.as_view(), name='process_already_answer'),
 ]
--- mm_evaluation/views.py
+++ mm_evaluation/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.db import IntegrityError
 
-from .models import Process, Macroprocess, Autoevaluation, Answer
+from .models import Process, Macroprocess, Autoevaluation, Answer, PYME
 from .forms import PunctuateProcessForm
 
 class AutoevaluationView(ListView):
@@ -14,19 +14,51 @@ class AutoevaluationView(ListView):
     template_name = 'mm_evaluation/autoevaluation.html'
     context_object_name = 'macroprocesses_list'
 
+    def is_autoevaluation_filled(self, a):
+        if a.final_score == None:
+            return False
+        return True
+
+    def get_autoevaluation(self):
+        # When login is working, this should be edited accordingly. pyme_id in the filter query is the id of the pyme that is filling que autoevaluation.
+        autoevaluations_list = Autoevaluation.objects.filter(pyme_id=1).order_by('start_time')
+        for autoevaluation in autoevaluations_list:
+            if not self.is_autoevaluation_filled(autoevaluation):
+                return autoevaluation
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        allow_empty = self.get_allow_empty()
+
+        if not allow_empty:
+            # When pagination is enabled and object_list is a queryset,
+            # it's better to do a cheap query than to load the unpaginated
+            # queryset in memory.
+            if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
+                is_empty = not self.object_list.exists()
+            else:
+                is_empty = not self.object_list
+            if is_empty:
+                raise Http404(_('Empty list and “%(class_name)s.allow_empty” is False.') % {
+                    'class_name': self.__class__.__name__,
+                })
+        context = self.get_context_data()
+        return self.render_to_response(context)
+
+    def post(self, request, pk):
+        autoevaluation = self.get_autoevaluation()
+        process = get_object_or_404(Process, pk=pk)
+        try:
+            answer = Answer(autoevaluation_id=autoevaluation, process_id=process, score=request.POST['score'])
+            answer.save()
+        except (IntegrityError):
+            return HttpResponseRedirect(reverse('mm_evaluation:process_already_answer'))
+        else:
+            # Always return an HttpResponseRedirect after successfully dealing
+            # with POST data. This prevents data from being posted twice if a
+            # user hits the Back button.
+            return HttpResponseRedirect(reverse('mm_evaluation:autoevaluation'))
+
+
 class ProcessAlreadyAnswerView(TemplateView):
     template_name = 'mm_evaluation/process_already_answer.html'
-
-def save_answer(request, pk):
-    autoevaluation = get_object_or_404(Autoevaluation, pk=1)
-    process = get_object_or_404(Process, pk=pk)
-    try:
-        answer = Answer(autoevaluation_id=autoevaluation, process_id=process, score=request.POST['score'])
-        answer.save()
-    except (IntegrityError):
-        return HttpResponseRedirect(reverse('mm_evaluation:process_already_answer'))
-    else:
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('mm_evaluation:autoevaluation'))
