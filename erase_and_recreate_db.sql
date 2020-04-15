916722a14313d89296ecd5d322ff65728f5fddea
Vincent Alejandro Arcila Larrea <jandro.vins@gmail.com>
2020-04-15 08:57:12 -0500

    Added character specs

--- erase_and_recreate_db.sql
+++ erase_and_recreate_db.sql
@@ -1,4 +1,4 @@
 DROP DATABASE diagnospymes;
-CREATE DATABASE diagnospymes;
+CREATE DATABASE diagnospymes CHARACTER SET utf8;
 GRANT ALL PRIVILEGES ON diagnospymes.* TO 'diagnospymes_user'@'localhost' IDENTIFIED BY 'password';
 flush privileges;
